8612dc092a0698d94063505e55e6133cbbd42e69
Acktarius <94026250+Acktarius@users.noreply.github.com>
2022-11-06 19:26:57 -0500

    Add files via upload

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,1 +1,28 @@
 //Check authentification
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+
+const checkAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated == true) { //(req.session.passport.user === authenticate)
+      return next();
+    } else {
+    res.redirect('/login');
+    console.log(req.isAuthenticated);
+    }
+  };
+
+const checkNotAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated == true) {
+      return res.redirect('/main');
+    } else {
+      return next();
+      console.log(req.isAuthenticated);
+    }
+  };
+
+  
+
+module.exports = { checkAuthenticated , checkNotAuthenticated };
+
