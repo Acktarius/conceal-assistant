0b85433c5630e14db2d0799f8b62fa7d7cbf737f
acktarius <xchaps@gmail.com>
2024-02-16 09:38:49 -0500

    bat file for windows

---
+++ ccx-assistant.bat
@@ -0,0 +1,15 @@
+@echo off
+:: set path to Conceal Assistant in next line
+cd "C:/conceal-assistant"
+Echo Conceal Assistant will start
+Echo Press 'Ctrl + c' to quit
+PAUSE
+CLS
+Echo ==========================
+Echo      Conceal Assistant
+Echo ==========================
+Echo Press 'Ctrl + c' to quit
+Echo,
+nodemon server.js
+
+:End 
