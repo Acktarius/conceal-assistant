da82f977a72e16a27c77394a2f1e8fe92baa4c3b
acktarius <xchaps@gmail.com>
2024-02-16 21:42:36 -0500

    bat&icon

--- ccx-assistant.bat
+++ ccx-assistant.bat
@@ -2,6 +2,7 @@
 :: set path to Conceal Assistant in next line
 cd "C:/conceal-assistant"
 Echo Conceal Assistant will start
+Echo,
 Echo Press 'Ctrl + c' to quit
 PAUSE
 CLS
@@ -11,5 +12,3 @@ Echo ==========================
 Echo Press 'Ctrl + c' to quit
 Echo,
 nodemon server.js
-
-:End 
